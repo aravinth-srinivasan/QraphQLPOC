8e21a272bfad9d68ba126c6999dc37c518a771cf
aravinth-srinivasan <aravinth.srinivasan@raweng.com>
2021-04-12 19:50:03 +0530

    remove client

--- app/src/main/java/com/raweng/graphqldemo/apolloClient.kt
+++
@@ -1,8 +0,0 @@
-package com.raweng.graphqldemo
-
-import com.apollographql.apollo.ApolloClient
-
-val apolloClient = ApolloClient.builder()
-    .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com")
-    .build()
-

8f6d30ccde126973bca2e313b2eef08ed0e5e95a
aravinth-srinivasan <aravinth.srinivasan@raweng.com>
2021-04-14 13:44:33 +0530

    removed error code

--- app/src/main/java/com/raweng/graphqldemo/todo/presentation/ToDoActivity.kt
+++ app/src/main/java/com/raweng/graphqldemo/todo/presentation/ToDoActivity.kt
@@ -1,22 +1,12 @@
 package com.raweng.graphqldemo.todo.presentation
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.apollographql.apollo.coroutines.await
-import com.apollographql.apollo.exception.ApolloException
-import com.example.LaunchListQuery
 import com.raweng.graphqldemo.R
-import com.raweng.graphqldemo.apolloClient
 import com.raweng.graphqldemo.databinding.ActivityTodoBinding
 import com.raweng.graphqldemo.utils.Resource
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ToDoActivity : DaggerAppCompatActivity() {
